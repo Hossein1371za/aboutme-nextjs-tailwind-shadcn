51dbfc0b92974903b2a7868a59ca182f6901d535
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-10-11 21:55:24 +0330

    services page & nav

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,24 +7,24 @@ import NavMobile from "./NavMobile";
 
 const Header = () => {
   return (
-    <header className="py-8 xl:py-12 text-white">
-      <div className="container mx-auto">
-        <Link href="/">
-          <h1 className="text-4xl font-semibold font-maghreb">
-            حسین <span className="text-accent">.</span>
-          </h1>
-        </Link>
+    <header className="py-8 xl:py-12 text-white font-maghreb">
+      <div className="container mx-auto flex justify-between items-center">
         {/* desktop nav */}
-        <div className="hidden xl:flex">
+        <div className="hidden xl:flex items-center gap-8">
           <Nav />
           <Link href="/contact">
-            <Button>ارتباط با من</Button>
+            <Button className="text-3xl">تماس با من</Button>
           </Link>
         </div>
         {/* mobile nav */}
         <div className="lg:hidden">
           <NavMobile />
         </div>
+        <Link href="/">
+          <h1 className="text-6xl font-semibold">
+            حسین <span className="text-accent">.</span>
+          </h1>
+        </Link>
       </div>
     </header>
   );
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,9 +1,26 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
+const links = [
+  { name: "خانه", path: "/" },
+  { name: "خدمات", path: "/services" },
+  { name: "رزومه", path: "/resume" },
+  { name: "کارها", path: "/work" },
+  { name: "ارتباط با من", path: "/contact" },
+];
 
 const Nav = () => {
+  const pathname = usePathname()
   return (
-    <nav>desktop nav</nav>
-  )
-}
-
-export default Nav
+    <nav className="flex gap-8">
+      {links.map((link, index) => (
+        <Link href={link.path} key={index} className={`${link.path === pathname && "text-accent border-accent border-b-2 "}font-medium hover:text-accent transition-all text-4xl`}>
+          {link.name}
+        </Link>
+      ))}
+    </nav>
+  );
+};
+  
+export default Nav;
