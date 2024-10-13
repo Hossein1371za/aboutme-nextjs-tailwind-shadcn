fdc8b2e425d667514d7dffd6a7937ce46cdf1af8
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-10-13 19:42:02 +0330

    animation change page

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,13 +1,16 @@
 import Header from "@/components/Header";
 import "./globals.css";
+import PageTransition from "@/components/PageTransition";
+import StairTransition from "@/components/StairTransition";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
       <body>
-        <Header/>
-        {children}
-        </body>
+        <Header />
+        <StairTransition />
+        <PageTransition>{children}</PageTransition>
+      </body>
     </html>
   );
 }
