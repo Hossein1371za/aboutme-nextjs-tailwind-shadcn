a5c693463e4e2cb489458b3c3cada80fa99a9579
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-10-15 19:21:59 +0330

    navmobile body

--- components/NavMobile.jsx
+++ components/NavMobile.jsx
@@ -1,9 +1,28 @@
+"use client";
 
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { CiMenuFries } from "react-icons/ci";
+
+const links = [
+  { name: "خانه", path: "/" },
+  { name: "خدمات", path: "/services" },
+  { name: "رزومه", path: "/resume" },
+  { name: "کارها", path: "/work" },
+  { name: "ارتباط با من", path: "/contact" },
+];
 
 const NavMobile = () => {
+  const pathname = usePathname();
   return (
-    <div>NavMobile</div>
-  )
-}
+    <Sheet>
+      <SheetTrigger className="flex justify-center items-center">
+        <CiMenuFries className="text-[32px] text-accent" />
+      </SheetTrigger>
+      <SheetContent>content</SheetContent>
+    </Sheet>
+  );
+};
 
-export default NavMobile
+export default NavMobile;
