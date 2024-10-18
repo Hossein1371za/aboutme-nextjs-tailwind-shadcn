f9fd0cec98e3e83ba380055dadffadf514647bc5
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-10-18 18:17:50 +0330

    photo

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,7 @@ import { FiDownload } from "react-icons/fi";
 
 // components
 import Socials from "@/components/Socials";
+import Photo from "@/components/Photo";
 
 const Home = () => {
   return (
@@ -37,7 +38,9 @@ const Home = () => {
             </div>
           </div>
           {/* photo */}
-          <div>photo</div>
+          <div>
+            <Photo />
+          </div>
         </div>
       </div>
     </section>
