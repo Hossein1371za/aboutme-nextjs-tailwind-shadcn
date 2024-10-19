d6dc743c484b5ec6eb723631fbb1f0a8284c2953
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-10-19 20:36:33 +0330

    stats use countup

--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,7 @@ import { FiDownload } from "react-icons/fi";
 // components
 import Socials from "@/components/Socials";
 import Photo from "@/components/Photo";
+import Stats from "@/components/Stats";
 
 const Home = () => {
   return (
@@ -11,13 +12,13 @@ const Home = () => {
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/* text */}
-          <div className="text-center xl:text-right order-2 xl:order-none">
+          <div className="text-center xl:text-right order-2 lg:order-none">
             <span className="text-xl">فرانت اند دولوپر</span>
             <h1 className="h1 mb-6">
               سلام من <br /> <span className="text-accent">حسین زحمتکشان</span>
             </h1>
             <p className="max-w-[580px] mb-9 text-white/80">
-             حدود 2 ساله که در حال آموزش فرانت اند هستم و تو این مدت تونستم
+              حدود 2 ساله که در حال آموزش فرانت اند هستم و تو این مدت تونستم
               قالب هایی با جدیدترین تکنولوژی های مرتبط طراحی کنم.
             </p>
             <div className="flex flex-col xl:flex-row items-center gap-8">
@@ -38,11 +39,12 @@ const Home = () => {
             </div>
           </div>
           {/* photo */}
-          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+          <div className="order-1 lg:order-none mb-8 xl:mb-0">
             <Photo />
           </div>
         </div>
       </div>
+      <Stats />
     </section>
   );
 };
---
+++ components/Stats.jsx
@@ -0,0 +1,37 @@
+"use client";
+
+import CountUp from "react-countup";
+
+const stats = [
+  { num: 2, text: "سال آموزش" },
+  { num: 18, text: "پروژه فرانت" },
+  { num: 9, text: "ابزار مرتبط با فرانت" },
+  { num: 446, text: "کد کامیت شده" },
+];
+
+const Stats = () => {
+  return (
+    <section className="pt-4 pb-12 xl:pt-0 xl:pb-0">
+      <div className="container mx-auto">
+        <div className="flex flex-wrap gap-6 max-v-[80vw] mx-auto xl:max-w-none">
+          {stats.map((stat, i) => (
+            <div
+              className="flex flex-1 gap-4 items-center justify-center xl:justify-start  font-maghreb"
+              key={i}
+            >
+              <CountUp
+                end={stat.num}
+                delay={2}
+                duration={5}
+                className="text-4xl xl:text-6xl font-extrabold text-accent"
+              />
+              <p className={`${stat.text.length < 15 ?"max-w-[100px]" : "max-w-[150px]"}text-4xl text-white/80`}>{stat.text}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Stats;
