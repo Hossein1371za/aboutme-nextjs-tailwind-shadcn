a2c5ef05b0541b2d1c272a21b06a6301d43cdbdf
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-10-22 22:32:38 +0330

    services

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,65 +1,72 @@
-"use client"
+"use client";
 
-import {BsArrowDownLeft} from "react-icons/bs"
-import Link from "next/link"
+import { BsArrowDownLeft } from "react-icons/bs";
+import Link from "next/link";
 
 const services = [
   {
-    num:"01",
-    title:"توسعه دهنده ی وب",
-    description:"لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان",
-    href:""
+    num: "01",
+    title: "توسعه دهنده ی وب",
+    description:
+      "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان",
+    href: "",
   },
   {
-    num:"02",
-    title:"طراحی زابط کاربری",
-    description:"لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان",
-    href:""
+    num: "02",
+    title: "طراحی رابط کاربری",
+    description:
+      "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان",
+    href: "",
   },
   {
-    num:"03",
-    title:"طراحی لوگو",
-    description:"لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان",
-    href:""
+    num: "03",
+    title: "طراحی لوگو",
+    description:
+      "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان",
+    href: "",
   },
   {
-    num:"04",
-    title:"سئو",
-    description:"لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان",
-    href:""
+    num: "04",
+    title: "سئو",
+    description:
+      "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان",
+    href: "",
   },
-]
+];
 
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
 const Services = () => {
   return (
-    <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
+    <section className="min-h-[80vh] font-maghreb flex flex-col justify-center py-12 xl:py-0">
       <div className="container mx-auto">
         <motion.div
-        initial={{opacity:0}}
-        animate={{
-          opacity:1,
-          transition:{delay:2.4,duration:0.4,ease:"easeIn"}
-        }}
+          className="grid grid-cols-1 md:grid-cols-2 gap-[60px]"
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+          }}
         >
-          {services.map((service)=>(
-            <div key={service.num}>
-              <div>
-                <div>{service.num}</div>
-                <Link href={service.href}>
-                  <BsArrowDownLeft />
+          {services.map((service) => (
+            <div key={service.num}
+          className="flex-1 flex flex-col justify-center gap-6 group">
+              <div className="flex w-full justify-between items-center">
+                <div className="text-6xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">{service.num}</div>
+                <Link href={service.href}
+                className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:rotate-45">
+                  <BsArrowDownLeft className="text-primary text-3xl"/>
                 </Link>
               </div>
-              <h2>{service.title}</h2>
-              <p>{service.description}</p>
+              <h2 className="text-[42px] font-bold group-hover:text-accent transition-all duration-500">{service.title}</h2>
+              <p className="text-white/60 text-2xl">{service.description}</p>
               <div className="border-b border-white/20 w-full"></div>
             </div>
           ))}
         </motion.div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
